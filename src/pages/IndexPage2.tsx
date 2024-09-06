dc5467ddb3400417c6bbe0c8e821afc119c3224c
rwiv <zc798546@gmail.com>
2024-09-06 18:35:55 +0900

    test: add MediaStream test

--- src/pages/IndexPage2.tsx
+++ src/pages/IndexPage2.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 interface ChatRoom {
   id: string;
@@ -7,9 +7,14 @@ interface ChatRoom {
 
 export function IndexPage2() {
   const [data, setData] = useState<ChatRoom[]>([]);
+  const recordedChunks = useRef<Blob[]>([]);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const anchorRef = useRef<HTMLAnchorElement | null>(null);
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
 
   useEffect(() => {
     req();
+    createMediaStream();
   }, []);
 
   const req = async () => {
@@ -19,11 +24,58 @@ export function IndexPage2() {
     setData(json);
   }
 
+  const createMediaStream = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      audio: true,
+    });
+
+    // const stream = await navigator.mediaDevices.getUserMedia({
+    //   video: true,
+    //   audio: true,
+    // });
+
+    const recorder = new MediaRecorder(stream, {mimeType: "video/webm"});
+    // recorder.start(1000);
+    mediaRecorder.current = recorder;
+
+    recorder.ondataavailable = event => {
+      console.log("asd")
+      if (event.data.size > 0) {
+        recordedChunks.current.push(event.data);
+      }
+    };
+
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream;
+    }
+    console.log(stream);
+  }
+
+  const onDlClick = () => {
+    mediaRecorder.current?.stop();
+    const blob = new Blob(recordedChunks.current, {
+      type: 'video/webm'
+    });
+    const url = URL.createObjectURL(blob);
+    const anchor = anchorRef.current;
+    if (anchor) {
+      anchor.href = url;
+      anchor.download = 'test.webm';
+      anchor.click();
+      URL.revokeObjectURL(url);
+    }
+    recordedChunks.current = [];
+  }
+
   return (
     <div>
       {data.map((chatRoom) => (
         <div key={chatRoom.id}>{chatRoom.title}</div>
       ))}
+      <a ref={anchorRef} style={{display: 'none'}}></a>
+      <button onClick={onDlClick}>download</button>
+      <video ref={videoRef} autoPlay/>
     </div>
   )
 }
